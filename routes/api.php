664d99817458b6d8d75676818ae81051c16b7188
Johnny Jose <johnnybonaci@gmail.com>
2025-10-08 13:39:53 -0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\AccessTokenController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/*
+|--------------------------------------------------------------------------
+| Access Token
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::post('/token', [AccessTokenController::class, 'token']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/tokens', [AccessTokenController::class, 'index']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
