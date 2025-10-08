b03772ddbda2502418e193f0ee6e58f35e7f72bf
Johnny Jose <johnnybonaci@gmail.com>
2025-10-08 13:29:54 -0300

    migrate V12 Auth

---
+++ app/Http/Api/Auth/AccessTokenController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Repositories\Auth\AccessTokenRepository;
+
+class AccessTokenController extends Controller
+{
+    /**
+     * __construct.
+     */
+    public function __construct(private AccessTokenRepository $access_token_repository) {}
+
+    /**
+     * Summary of token.
+     */
+    public function token(Request $request): JsonResponse
+    {
+        $response = $this->access_token_repository->response();
+        $code = 401;
+        $token = $this->access_token_repository->basicToken($request);
+        if ($token) {
+            $accessToken = $this->access_token_repository->getPersonalAccessToken($token);
+
+            if ($accessToken) {
+                if ($accessToken->can('token:read')) {
+                    $this->access_token_repository->deleteAccessToken($accessToken);
+                    $newToken = $this->access_token_repository->createAccessToken($accessToken);
+                    $response = $this->access_token_repository->response($newToken, true);
+                    $code = 200;
+                }
+            }
+        }
+
+        return response()->json($response, $code);
+    }
+}
---
+++ app/Repositories/Auth/AccessTokenRepository.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Repositories\Auth;
+
+use Illuminate\Http\Request;
+use Laravel\Sanctum\PersonalAccessToken;
+
+class AccessTokenRepository
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get the basic token from the request headers.
+     */
+    public function basicToken(Request $request): ?string
+    {
+        $header = $request->header('Authorization', '');
+
+        $position = strrpos($header, 'Basic');
+
+        if ($position !== false) {
+            $header = substr($header, $position + 6);
+
+            return str_contains($header, ',') ? strstr($header, ',', true) : $header;
+        }
+
+        return null;
+    }
+
+    /**
+     * Summary of getPersonalAccessToken.
+     */
+    public function getPersonalAccessToken(string $token): ?PersonalAccessToken
+    {
+        return PersonalAccessToken::findToken($token);
+    }
+
+    /**
+     * Summary of findAccessToken.
+     */
+    public function updatedAccessToken(PersonalAccessToken $personalAccessToken): PersonalAccessToken
+    {
+        $personalAccessToken->forceFill([
+            'last_used_at' => now(),
+            'expires_at' => now()->addDays(1),
+        ])->save();
+
+        return $personalAccessToken;
+    }
+
+    /**
+     * Summary of deleteAccessToken.
+     */
+    public function deleteAccessToken(PersonalAccessToken $personalAccessToken): void
+    {
+        PersonalAccessToken::where('name', 'api-' . $personalAccessToken->tokenable->email)->delete();
+    }
+
+    /**
+     * Summary of createAccessToken.
+     */
+    public function createAccessToken(PersonalAccessToken $personalAccessToken): string
+    {
+        return $personalAccessToken->tokenable->createToken('api-' . $personalAccessToken->tokenable->email, ['lead.api:read', 'lead.api:create'], now()->addDays(1))->plainTextToken;
+    }
+
+    /**
+     * Summary of response.
+     */
+    public function response(string|bool $token = false, bool $success = false): array
+    {
+        if ($success && $token) {
+            return [
+                'status' => 'success',
+                'message' => __('Logged in successfully.'),
+                'token_code' => $token,
+                'token_type' => 'Bearer',
+                'expires_in' => 86400,
+            ];
+        }
+
+        return [
+            'status' => 'errors',
+            'message' => 'The provided credentials are incorrect.',
+        ];
+    }
+}
